6899cc31d78b677640d975059caf414b33a89f86
Sergiu Dumitriu <sdumitriu@users.noreply.github.com>
2018-02-07 12:51:48 -0500

    [misc] Fixed wrong import

--- sender/api/src/main/java/org/phenotips/integration/lims247/internal/RemoteSynchronizationEventListener.java
+++ sender/api/src/main/java/org/phenotips/integration/lims247/internal/RemoteSynchronizationEventListener.java
@@ -43,7 +43,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.apache.http.Consts;
